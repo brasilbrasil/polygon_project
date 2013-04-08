408008a1daf4d85db3dca0193ad788d04711e30b
Lucas Fortini <brasilbrasil@gmail.com>
2013-04-08 08:21:35 -1000

    Added test py file

---
+++ resample_multiple_rasters.py
@@ -0,0 +1,7 @@
+import arcpy
+rootdir="Y:/Test files/"
+arcpy.env.workspace = rootdir
+rasterList = arcpy.ListRasters("*", "tif")
+for raster in rasterList:    
+    arcpy.Resample_management(rootdir+raster, rootdir+raster[:-4]+"_1000m.tif", "1000", "NEAREST")
+    
