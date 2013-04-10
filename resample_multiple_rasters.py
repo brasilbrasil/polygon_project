77e6a671904d6579e9f8d10f23b2303223c33bf4
Lucas Fortini <brasilbrasil@gmail.com>
2013-04-10 13:29:01 -1000

    test commit2

--- resample_multiple_rasters.py
+++ resample_multiple_rasters.py
@@ -5,4 +5,3 @@ rasterList = arcpy.ListRasters("*", "tif")
 for raster in rasterList:    
     arcpy.Resample_management(rootdir+raster, rootdir+raster[:-4]+"_1000m.tif", "1000", "NEAREST")
 #test comment
-#this a second comment
